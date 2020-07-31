9d6f8d2526b8c23a2fa47e31d7aaaa8847061045
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-31 11:45:56 +0700

    Add files via upload

--- Week3.cpp
+++ Week3.cpp
@@ -1,37 +1,17 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
-void add(int,int); // ประกาศฟังก์ชัน
-void sub(int,int);
-void mul(int,int);
-void dev(int,int);
-
+void F_func(float);
 int main()
 {
-	int x , y ; // local variable
-	cout << "Enter X : " ;
-	cin >> x ;
-	cout << "Enter Y : " ;
-	cin >> y ;
-	add(x,y); // เรียกใช้ฟังก์ชัน
-	sub(x,y);
-	mul(x,y);
-	dev(x,y);
-	system("pause");
-	return 0 ;
+	float C ;
+	cout<<"Enter Celsius : ";
+	cin>>C;
+	F_func(C);
+	return 0;
 }
-void add(int a,int b) // การสร้างฟังก์ชัน
-{	
-	cout << a << "+" << b << "=" << a+b << endl ;
-}
-void sub(int a,int b)
+void F_func(float C)
 {
-	cout<<a<<"-"<<b<<"="<<a-b<<endl;
-}
-void mul(int a,int b)
-{
-	cout<<a<<"*"<<b<<"="<<a*b<<endl;
-}
-void dev(int a,int b)
-{
-	cout<<a<<"/"<<b<<"="<<a/b<<endl;
+	float F;
+	F=C*1.8+32;
+	cout<<"The Fahrenheit is "<<(F>=70 ? "HOT":"COOL")<<endl;
 }
