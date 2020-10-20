c60814100b3befc4774087641444a9d378695947
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-10-20 10:17:40 +0700

    Add files via upload

---
+++ File.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<time.h>
+using namespace std;
+int main()
+{
+	ofstream outfile;
+	//int value;
+	string name;
+	int mscore,fscore;
+	string filename="myfile2.txt";
+	outfile.open(filename.c_str(),ios_base::out|ios_base::app);
+	srand(time(NULL));
+	if(outfile.is_open())
+	{
+		cout<<"DD.\n";
+		for(int i=0;i<=3;i++)
+		{
+			//value=rand()%100;
+			//cout<<value<<endl;
+			//outfile<<value<<" ";
+			cout<<"Enter name : ";
+			cin>>name;
+			cout<<"Enter mid-score : ";
+			cin>>mscore;
+			cout<<"Enter final-score : ";
+			cin>>fscore;
+			outfile<<name<<" "<<mscore<<" "<<fscore;
+
+		}
+	}
+	else
+	{
+		cout<<"EE.\n";
+	}
+	outfile.close();
+	ifstream infile;
+	infile.open(filename.c_str());
+	if(infile.is_open())
+	{
+		cout<<"DD.\n";
+		while(infile>>name>>mscore>>fscore)
+		{
+			cout<<name<<" "<<mscore<<" "<<fscore<<endl;
+		}
+	}
+	else
+	{
+		cout<<"EE.\n";
+	}
+
+	system("pause"); 
+	return 0;
+}
